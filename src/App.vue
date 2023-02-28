10812ac93f6944c9983c4460b9cfcc3fc4fa20c4
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2023-02-28 12:35:14 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+    import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <header>
+        <nav>
+          <ul>
+            <li>
+              <RouterLink to="/">Home</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/about">About</RouterLink>
+            </li>
+          </ul>
+        </nav>
+    </header>
+
+    <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>
+    <p style="margin-bottom: 2em">
+      Weeeeee! <br>
+      This is just a front page :3 <br>
+      Plz use my software
+    </p>
+    <p>
+      Me me like pineapples 🍍
+    </p>
+  </p>
+</template>
