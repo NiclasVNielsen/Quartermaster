94b3ec1a1f33dc2c31e14cbb37329bc1c8bfd07b
NiclasVNielsen <Niclasvnielsen@gmail.com>
2023-03-07 17:31:55 +0100

    Restart

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,9 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
   'extends': [
     'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript'
-  ],
-  overrides: [
-    {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
+    'eslint:recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
