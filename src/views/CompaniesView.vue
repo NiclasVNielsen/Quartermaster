482b7d82ee56709816b8788c7311e4494d29aef5
NiclasVNielsen <57449509+NiclasVNielsen@users.noreply.github.com>
2023-04-18 09:39:11 +0200

    create popups

--- src/views/CompaniesView.vue
+++ src/views/CompaniesView.vue
@@ -1,13 +1,24 @@
 <script setup>
 import Header from '../components/HeaderComponent.vue'
+
+const createPopup = () => {
+  const popup = document.querySelector(".popup")
+
+  popup.classList.toggle("off");
+}
 </script>
 
 <template>
   <Header />
   <main>
-    <button>
+    <button @click="createPopup">
       Create Company
     </button>
+    <div class="popup off" @click="createPopup">
+      <div class="popupForm" @click.stop>
+        Meep moop!
+      </div>
+    </div>
     
   </main>
 </template>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,17 +1,48 @@
 <script setup>
 import Header from '../components/HeaderComponent.vue'
+
+const createPopup = () => {
+  const popup = document.querySelector(".popup")
+
+  popup.classList.toggle("off");
+}
 </script>
 
 <template>
   <Header />
   <main>
-    <button>
+    <button @click="createPopup">
       Create Project
     </button>
+    <div class="popup off" @click="createPopup">
+      <div class="popupForm" @click.stop>
+        Meep moop!
+      </div>
+    </div>
     
   </main>
 </template>
 
 <style lang="sass">
-    
+
+.popup.off
+  display: none
+
+.popup
+  position: absolute
+  top: 0
+  left: 0
+  width: 100%
+  height: 100vh
+  background: #000
+  opacity: 0.3
+  .popupForm
+    position: absolute
+    top: 50%
+    left: 50%
+    transform: translate(-50%, -50%)
+    width: 500px
+    height: 400px
+    background: #fff
+
 </style>
